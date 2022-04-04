daeb03e90f2373dd07af349e7f4ae6940d55def8
jb <a@b.com>
2022-04-04 16:48:57 -0700

    Initial check in.

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import PkgCalc.Calculator;
+
+public class Demo{
+   public static void main(String args[]){
+      
+   	Calculator obj = new Calculator();
+   	System.out.println(obj.add(100, 200));
+   	System.out.println(obj.add(321, 456));
+   }
+}
